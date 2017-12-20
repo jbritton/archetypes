848a0031cccfa4e047a5c6072c17f62b9043ea43
Jeff Britton <britton.jeff@gmail.com>
2017-12-19 22:05:45 -0500

    Reformatting code

--- angular-web-archetype/src/index.js
+++ angular-web-archetype/src/index.js
@@ -2,8 +2,6 @@ import angular from 'angular';
 import './index.scss';
 import appModule from './example.app.js';
 
-
-
 // bootstrap the application
 const rootElement = document.getElementById('root');
 angular.bootstrap(rootElement, [ appModule.name ]);
